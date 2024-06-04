c3bfa6c50647e828f021774fc9866ba7400a1e13
Mahesh Warang <76471375+maheshwarang001@users.noreply.github.com>
2024-06-04 17:35:20 +0530

    .

--- src/main/java/org/example/lvl1/UltraFastMathematician.java
+++ src/main/java/org/example/lvl1/UltraFastMathematician.java
@@ -1,4 +1,22 @@
 package org.example.lvl1;
 
+import java.util.Scanner;
+
 public class UltraFastMathematician {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        for(int i = 0 ; i <s1.length() ; i++){
+            if(s1.charAt(i) == s2.charAt(i)){
+                System.out.print("0");
+            }else{
+                System.out.print("1");
+            }
+        }
+    }
 }
